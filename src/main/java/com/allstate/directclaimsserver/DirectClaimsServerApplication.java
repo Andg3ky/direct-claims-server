532df15c3036b22a8891950bbde8344f5cc2e86f
Andg3ky <andrewleah55@outlook.com>
2022-08-03 12:45:56 +0000

    first commit

---
+++ src/main/java/com/allstate/directclaimsserver/DirectClaimsServerApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.directclaimsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectClaimsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectClaimsServerApplication.class, args);
+	}
+
+}
